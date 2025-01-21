912915f6cd7b4aa9e676a6bb49e780e976a01cce
rhongobongooo <epicjeff03@gmail.com>
2025-01-21 08:58:16 +0800

    WIP: base login and registration pages

--- src/app/(auth)/login/page.tsx
+++ src/app/(auth)/login/page.tsx
@@ -1,11 +1,30 @@
 "use client"
 
-import React from 'react';
+import { auth, signInWithEmailAndPassword } from '@/lib/Firebase/Firebase';
+import { loginWithCredentials } from '@/lib/APICalls/Auth/login';
+import React, {
+    useState
+} from 'react';
 
 const LoginPage: React.FC = () => {
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = async (e: React.FormEvent) => {
+        e.preventDefault();
+        try {
+            const userCredential = await signInWithEmailAndPassword(auth, email, password);
+            await loginWithCredentials(await userCredential.user.getIdToken())
+        } catch (error) {
+            console.log(error);
+        }
+    }
+
     return (
         <div className="flex items-center justify-center h-screen bg-gray-100">
-            <form className="w-full max-w-sm bg-white p-6 rounded-lg shadow-md">
+            <form className="w-full max-w-sm bg-white p-6 rounded-lg shadow-md"
+                onSubmit={handleSubmit}>
                 <h2 className="text-2xl font-semibold text-center mb-6">Login</h2>
 
                 <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
@@ -16,6 +35,7 @@ const LoginPage: React.FC = () => {
                     id="email"
                     placeholder="Enter your email"
                     className="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+                    onChange={(e) => setEmail(e.target.value)}
                 />
 
                 <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
@@ -26,6 +46,7 @@ const LoginPage: React.FC = () => {
                     id="password"
                     placeholder="Enter your password"
                     className="w-full px-3 py-2 mb-6 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
+                    onChange={(e) => setPassword(e.target.value)}
                 />
 
                 <button
