e45000c5480b279bd5a0e7275dd85f6a1452a906
rhongobongooo <epicjeff03@gmail.com>
2025-01-21 13:24:39 +0800

    fix: disabled no-unused-vars

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  rules: {
+    '@typescript-eslint/no-unused-vars': 'off',
+  },
 };
 
 export default nextConfig;
