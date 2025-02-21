77b9d841af2521f6eabac2b1a394302a489bebe8
rhongobongooo <epicjeff03@gmail.com>
2025-02-21 13:41:59 +0800

    feat(RBAC): show role in landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,10 @@ export const dynamic = 'force-dynamic';
 import Dashboard from "@/components/example/SSRComponentExample";
 
 export default function Home() {
-  return (
-    <div>
-      <Dashboard></Dashboard>
-      <p>LANDING PAGE?????</p>
-    </div>
-  );
+    return (
+        <div>
+            <Dashboard></Dashboard>
+            <p>LANDING PAGE?????</p>
+        </div>
+    );
 }
