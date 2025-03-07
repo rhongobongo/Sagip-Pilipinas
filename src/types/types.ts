6bfeeb6030104fa57174bcb96a4d3b001e0dcc76
M4monski <162318730+M4monski@users.noreply.github.com>
2025-03-07 22:41:42 +0800

    refactor(SubmitAid.ts & types.ts): added new Types

--- src/types/types.ts
+++ src/types/types.ts
@@ -1,18 +1,20 @@
 import type { Timestamp } from "firebase/firestore";
 
 export interface DefaultPin {
-    coordinates: { latitude: number; longitude: number };
+  coordinates: { latitude: number; longitude: number };
 }
 
 export interface MainPin extends DefaultPin {
-    id: string;
-    disasterType?: string;
-    date?: Timestamp;
+  id: string;
+  disasterType?: string;
+  date?: Timestamp;
 }
 
 export interface RequestPin extends DefaultPin {
-    fullName?: string;
-    contactNumber?: string;
-    disasterType?: string;
-    aidType?: string;
+  name?: string;
+  contactNum?: string;
+  date?: Timestamp;
+  calamityLevel?: string;
+  calamityType?: string;
+  shortDesc?: string;
 }
