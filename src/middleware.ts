eb1cc272c1eed2762e14f2a301be38569e6f8bc5
rhongobongooo <epicjeff03@gmail.com>
2025-03-12 09:26:36 +0800

    feat(RBAC): base role-based access in middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,15 +1,21 @@
-import { authConfig } from "@/lib/Next-Firebase-Auth-Edge/NextFirebaseAuthEdge";
-import { NextResponse, 
-        NextRequest } from "next/server";
-import { authMiddleware,
-    redirectToLogin } from "next-firebase-auth-edge";
-import { headers } from "next/headers";
+import MiddlewareFirebaseAdmin, { authConfig } from "@/lib/Next-Firebase-Auth-Edge/NextFirebaseAuthEdge";
+import {
+    NextResponse,
+    NextRequest
+} from "next/server";
+import {
+    authMiddleware,
+    redirectToLogin
+} from "next-firebase-auth-edge";
 
-const PUBLIC_PATHS = ['/', '/about', '/contact-us', '/map'];
+const PUBLIC_PATHS = ['/', '/about', '/contact-us', '/map', '/request-aid', '/404'];
 const AUTH_PATHS = ['/register', '/login', 'forget-password'];
 
 export default async function middleware(request: NextRequest) {
-    
+
+    const adminRegex = /^\/admin(?:\/([a-zA-Z0-9]+))?(?:\/|$)/;
+    const { nextUrl } = request;
+
     return authMiddleware(request, {
         loginPath: '/api/login',
         logoutPath: '/api/logout',
@@ -22,12 +28,39 @@ export default async function middleware(request: NextRequest) {
         serviceAccount: authConfig.serviceAccount,
         enableCustomToken: authConfig.enableCustomToken,
 
-        handleValidToken: async ({token, decodedToken}, headers) => {
+        handleValidToken: async ({ token, decodedToken }, headers) => {
+
+            if (adminRegex.test(nextUrl.pathname)) {
+                const user = await MiddlewareFirebaseAdmin.getUser(decodedToken.uid);
+                if (!user?.customClaims?.admin) {
+                    return NextResponse.redirect(new URL('/404', request.url));
+                }
+            }
             return NextResponse.next({
                 request: {
-                  headers,
+                    headers,
                 },
-              });
+            });
+        },
+
+        handleInvalidToken: async (_reason) => {
+            if (adminRegex.test(nextUrl.pathname)) {
+                return NextResponse.redirect(new URL('/404', request.url));
+            }
+            return redirectToLogin(request, {
+                path: '/login',
+                publicPaths: [...PUBLIC_PATHS, ...AUTH_PATHS]
+            })
         }
     })
 }
+
+export const config = {
+    matcher: [
+        '/',
+        '/((?!_next|favicon.ico|__/auth|__/firebase|.*\\.).*)',
+        '/api/login',
+        '/api/logout',
+        '/api/refresh-token'
+    ]
+};
