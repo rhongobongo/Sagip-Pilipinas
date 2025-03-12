c9ae063234f9c9692337370450ee68d09635a03e
rhongobongooo <epicjeff03@gmail.com>
2025-03-12 09:26:36 +0800

    feat(RBAC): base role-based access in middleware

--- src/lib/Next-Firebase-Auth-Edge/NextFirebaseAuthEdge.ts
+++ src/lib/Next-Firebase-Auth-Edge/NextFirebaseAuthEdge.ts
@@ -1,17 +1,17 @@
 import { ReadonlyRequestCookies } from "next/dist/server/web/spec-extension/adapters/request-cookies";
-import { getTokens } from "next-firebase-auth-edge";
+import { getTokens, getFirebaseAuth } from "next-firebase-auth-edge";
 
 export const serverConfig = {
     useSecureCookies: false,
     firebaseApiKey: process.env.FIREBASE_API_KEY ?? '',
     serviceAccount: {
-        projectId: process.env.FIREBASE_PROJECT_ID as string, 
+        projectId: process.env.FIREBASE_PROJECT_ID as string,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
         privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') as string,
     }
 };
-  
-  
+
+
 export const authConfig = {
     apiKey: serverConfig.firebaseApiKey,
     cookieName: 'AuthToken',
@@ -30,17 +30,28 @@ export const authConfig = {
     enableCustomToken: false,
 };
 
-export const getAuthTokens =  async (cookies : ReadonlyRequestCookies) => {
+export const getAuthTokens = async (cookies: ReadonlyRequestCookies) => {
     return await getTokens(cookies, {
         apiKey: serverConfig.firebaseApiKey,
         cookieName: 'AuthToken',
         cookieSignatureKeys: process.env.COOKIE_SIGNATURE_KEYS
-        ? process.env.COOKIE_SIGNATURE_KEYS.split('\n')
-        : [],
+            ? process.env.COOKIE_SIGNATURE_KEYS.split('\n')
+            : [],
         serviceAccount: {
-            projectId: process.env.FIREBASE_PROJECT_ID as string, 
+            projectId: process.env.FIREBASE_PROJECT_ID as string,
             clientEmail: process.env.FIREBASE_CLIENT_EMAIL as string,
             privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') as string,
         },
-      })
+    })
 };
+
+const MiddlewareFirebaseAdmin = getFirebaseAuth({
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY ?? "",
+    serviceAccount: {
+        projectId: process.env.FIREBASE_PROJECT_ID ?? "",
+        clientEmail: process.env.FIREBASE_CLIENT_EMAIL ?? "",
+        privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n') ?? "",
+    }
+});
+
+export default MiddlewareFirebaseAdmin;
