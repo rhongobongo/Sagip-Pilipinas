184dad4f8a6c0e7037583c45a97a3c4ddc2abcc6
rhongobongooo <epicjeff03@gmail.com>
2025-03-12 18:55:02 +0800

    refactor(home page): use container in map section for better responsiveness

--- src/components/home/MapSection/MapSectionInteractive.tsx
+++ src/components/home/MapSection/MapSectionInteractive.tsx
@@ -12,8 +12,8 @@ const MapSectionInteractive: React.FC<{ locations: OrganizationPin[] }> = ({ loc
 
 
     return (
-        <div className="grid grid-cols-2 gap-8 p-8 h-[80vh]">
-            <div className="mx-20 rounded-2xl flex-grow">
+        <div className="container mx-auto grid grid-cols-2 gap-8 p-8 h-[80vh]">
+            <div className="rounded-2xl flex-grow">
                 <DistributionMapHomeWrapper pinData={locations} selectedPin={selectedPin} />
             </div>
             <div className="h-full">
