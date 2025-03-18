a7b7772dbe1eca1f49fbd225af5ff2ce70a027b3
22-1918-942 Keith Harvey Angel <keithharvey.angel@cit.edu>
2025-03-18 19:51:41 +0800

    registrartion try

--- src/components/(page)/AuthPage/RegistrationForm.tsx
+++ src/components/(page)/AuthPage/RegistrationForm.tsx
@@ -1,87 +1,96 @@
-"use client"
+'use client';
 
 import { auth, createUserWithEmailAndPassword } from '@/lib/Firebase/Firebase';
 import { loginWithCredentials } from '@/lib/APICalls/Auth/login';
-import React, {
-    useState
-} from 'react';
-
+import React, { useState } from 'react';
 
 const RegistrationForm: React.FC = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
+  const setUpAccountInFireStore = async (idToken: string) => {
+    try {
+      const response = await fetch('/api/registration', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${idToken}`,
+        },
+      });
 
-    const setUpAccountInFireStore = async (
-        idToken: string,
-    ) => {
-        try {
-            const response = await fetch("/api/registration", {
-                method: "POST",
-                headers: {
-                    "Content-Type": "application/json",
-                    Authorization: `Bearer ${idToken}`,
-                },
-            });
-
-            if (!response.ok) {
-                const errorData = await response.json();
-                throw(errorData);
-            }
-        } catch (e) {
-            console.error("Error in setUpAccountInFireStore:", e);
-        }
-    };
-
-    const handleSubmit = async (e: React.FormEvent) => {
-        e.preventDefault();
-        try {
-            const userCredential = await createUserWithEmailAndPassword(auth, email, password);
-            await loginWithCredentials(await userCredential.user.getIdToken());
-            await setUpAccountInFireStore(await userCredential.user.getIdToken())
-
-        } catch (error) {
-            console.log(error);
-        }
+      if (!response.ok) {
+        const errorData = await response.json();
+        throw errorData;
+      }
+    } catch (e) {
+      console.error('Error in setUpAccountInFireStore:', e);
     }
+  };
 
-    return (
-        <div className="flex items-center justify-center h-screen bg-gray-100">
-            <form className="w-full max-w-sm bg-white p-6 rounded-lg shadow-md"
-                onSubmit={handleSubmit}>
-                <h2 className="text-2xl font-semibold text-center mb-6">Registration</h2>
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      await loginWithCredentials(await userCredential.user.getIdToken());
+      await setUpAccountInFireStore(await userCredential.user.getIdToken());
+    } catch (error) {
+      console.log(error);
+    }
+  };
 
-                <label htmlFor="email" className="block text-sm font-medium text-gray-700 mb-1">
-                    Email
-                </label>
-                <input
-                    type="email"
-                    id="email"
-                    placeholder="Enter your email"
-                    className="w-full px-3 py-2 mb-4 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-                    onChange={(e) => setEmail(e.target.value)}
-                />
-
-                <label htmlFor="password" className="block text-sm font-medium text-gray-700 mb-1">
-                    Password
-                </label>
-                <input
-                    type="password"
-                    id="password"
-                    placeholder="Enter your password"
-                    className="w-full px-3 py-2 mb-6 border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-500 text-black"
-                    onChange={(e) => setPassword(e.target.value)}
-                />
-
-                <button
-                    type="submit"
-                    className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none"
-                >
-                    Login
-                </button>
-            </form>
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-100">
+      <form
+        className="w-[75%] bg-white p-6 rounded-lg shadow-md"
+        onSubmit={handleSubmit}
+      >
+        <h1 className="text-black flex justify-center">Join us now!</h1>
+        <div className="flex flex-grow-0 flex-shrink-0 gap-4 mt-4 mb-4 rounded-sm   ">
+          <div className="w-full">
+            <img src="/home-image/image9.jpg" alt="" />
+            <button
+              type="submit"
+              className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none mt-4"
+            >
+              Volunteer
+            </button>
+          </div>
+          <div className="w-full">
+            <img src="/home-image/image1.jpg" alt="" className="w-full" />
+            <button
+              type="submit"
+              className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none mt-4"
+            >
+              Organization
+            </button>
+          </div>
         </div>
-    );
+
+        <h2 className="text-black flex justify-center">
+          Already have an account?
+        </h2>
+
+        <div className="grid gap-4 mt-4">
+          <button
+            type="submit"
+            className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none"
+          >
+            Login as Volunteer
+          </button>
+          <button
+            type="submit"
+            className="w-full py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 focus:outline-none"
+          >
+            Login as Organization
+          </button>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default RegistrationForm;
--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,45 +1,48 @@
-import Link from "next/link";
+import Link from 'next/link';
 
 interface LinkInterface {
-    displayName: string;
-    href: string;
+  displayName: string;
+  href: string;
 }
 
 const linksArray: LinkInterface[] = [
-    { displayName: "Home", href: "/" },
-    { displayName: "Map", href: "/map" },
-    { displayName: "Request Aid", href: "/request-aid" },
-    { displayName: "Learn More", href: "/learnmore" },
-    { displayName: "Sign Up", href: "/register" },
+  { displayName: 'Home', href: '/' },
+  { displayName: 'Map', href: '/map' },
+  { displayName: 'Request Aid', href: '/request-aid' },
+  { displayName: 'Learn More', href: '/learnmore' },
+  { displayName: 'Register Now', href: '/register' },
 ];
 
 const Navbar: React.FC = () => {
-    return (
-        <nav className="p-4 bg-[#B0022A] shadow-[inset_0_-1px_0_0_rgba(169,169,169,.5)]">
-            <div className="max-w-6xl mx-auto flex justify-between items-center">
-                
-                <div className="flex items-center space-x-3">
-                    <img src="/logo.png" alt="Logo" className="h-10 w-10 object-contain" />
-                    <Link href="/" className="font-bold text-lg text-white">
-                        SAGIP PILIPINAS: HELP THOSE IN NEED!
-                    </Link>
-                </div>
-                
-                <ul className="flex space-x-6">
-                    {linksArray.map((link) => (
-                        <li key={link.href}>
-                            <Link
-                                href={link.href}
-                                className="text-white px-1.5 py-0.5 inline-block text-lg font-extralight hover:text-gray-200 duration-300"
-                            >
-                                {link.displayName}
-                            </Link>
-                        </li>
-                    ))}
-                </ul>
-            </div>
-        </nav>
-    );
+  return (
+    <nav className="p-4 bg-[#B0022A] shadow-[inset_0_-1px_0_0_rgba(169,169,169,.5)]">
+      <div className="max-w-6xl mx-auto flex justify-between items-center">
+        <div className="flex items-center space-x-3">
+          <img
+            src="/logo.png"
+            alt="Logo"
+            className="h-10 w-10 object-contain"
+          />
+          <Link href="/" className="font-bold text-lg text-white">
+            SAGIP PILIPINAS: HELP THOSE IN NEED!
+          </Link>
+        </div>
+
+        <ul className="flex space-x-6">
+          {linksArray.map((link) => (
+            <li key={link.href}>
+              <Link
+                href={link.href}
+                className="text-white px-1.5 py-0.5 inline-block text-lg font-extralight hover:text-gray-200 duration-300"
+              >
+                {link.displayName}
+              </Link>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
