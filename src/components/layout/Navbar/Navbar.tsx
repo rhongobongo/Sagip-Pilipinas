925a06a6c1b080ef5d2c7acc368451bfa8132d08
MarkToroy <markjohntoroy2016@gmail.com>
2025-03-28 20:16:34 +0800

    home page cleanup

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -16,24 +16,24 @@ const linksArray: LinkInterface[] = [
 const Navbar: React.FC = () => {
   return (
     <nav className="p-4 bg-[#B0022A] shadow-[inset_0_-1px_0_0_rgba(169,169,169,.5)]">
-      <div className="max-w-6xl mx-auto flex justify-between items-center">
-        <div className="flex items-center space-x-3">
+      <div className="max-w-8xl mx-auto flex justify-between items-center border-4 border-[#B0022A]">
+        <div className="flex items-center space-x-3 justify-end">
           <img
             src="/logo.png"
             alt="Logo"
-            className="h-10 w-10 object-contain"
+            className="h-10 w-10 object-contain ml-10"
           />
           <Link href="/" className="font-bold text-lg text-white">
             SAGIP PILIPINAS: HELP THOSE IN NEED!
           </Link>
         </div>
 
-        <ul className="flex space-x-6">
+        <ul className="flex space-x-6 mr-10">
           {linksArray.map((link) => (
             <li key={link.href}>
               <Link
                 href={link.href}
-                className="text-white px-1.5 py-0.5 inline-block text-lg font-extralight hover:text-gray-200 duration-300"
+                className="text-white px-1.5 py-0.5 inline-block text-lg font-extralight transition-all ease-in-out duration-300 hover:text-white hover:decoration-white hover:underline hover:decoration-2 hover:underline-offset-8 hover:scale-110"
               >
                 {link.displayName}
               </Link>
