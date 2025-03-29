f490fe4ab4be92be107b2bc11c2b6a6a815a586b
rhongobongooo <epicjeff03@gmail.com>
2025-03-30 05:26:19 +0800

    feat(navbar): remove register if user is already logged in

--- src/components/layout/Navbar/Navbar.tsx
+++ src/components/layout/Navbar/Navbar.tsx
@@ -1,48 +1,52 @@
-import Link from 'next/link';
+import Link from "next/link";
+import { useAuth } from "@/stores/AuthStores/AuthContext";
 
 interface LinkInterface {
-  displayName: string;
-  href: string;
+    displayName: string;
+    href: string;
 }
 
-const linksArray: LinkInterface[] = [
-  { displayName: 'Home', href: '/' },
-  { displayName: 'Map', href: '/map' },
-  { displayName: 'Request Aid', href: '/request-aid' },
-  { displayName: 'Learn More', href: '/learnmore' },
-  { displayName: 'Register Now', href: '/register' },
-];
+const Navbar = () => {
 
-const Navbar: React.FC = () => {
-  return (
-    <nav className="p-4 bg-[#B0022A] shadow-[inset_0_-1px_0_0_rgba(169,169,169,.5)]">
-      <div className="max-w-8xl mx-auto flex justify-between items-center border-4 border-[#B0022A]">
-        <div className="flex items-center space-x-3 justify-end">
-          <img
-            src="/logo.png"
-            alt="Logo"
-            className="h-10 w-10 object-contain ml-10"
-          />
-          <Link href="/" className="font-bold text-lg text-white">
-            SAGIP PILIPINAS: HELP THOSE IN NEED!
-          </Link>
-        </div>
+    const {user} = useAuth()
 
-        <ul className="flex space-x-6 mr-10">
-          {linksArray.map((link) => (
-            <li key={link.href}>
-              <Link
-                href={link.href}
-                className="text-white px-1.5 py-0.5 inline-block text-lg font-extralight transition-all ease-in-out duration-300 hover:text-white hover:decoration-white hover:underline hover:decoration-2 hover:underline-offset-8 hover:scale-110"
-              >
-                {link.displayName}
-              </Link>
-            </li>
-          ))}
-        </ul>
-      </div>
-    </nav>
-  );
+    const linksArray: LinkInterface[] = [
+        { displayName: "Home", href: "/" },
+        { displayName: "Map", href: "/map" },
+        { displayName: "Request Aid", href: "/request-aid" },
+        { displayName: "Learn More", href: "/learnmore" },
+        ...(!user ? [{ displayName: "Register Now", href: "/register" }] : []),
+    ];
+
+    return (
+        <nav className="p-4 bg-[#B0022A] shadow-[inset_0_-1px_0_0_rgba(169,169,169,.5)]">
+            <div className="max-w-8xl mx-auto flex justify-between items-center border-4 border-[#B0022A]">
+                <div className="flex items-center space-x-3 justify-end">
+                    <img
+                        src="/logo.png"
+                        alt="Logo"
+                        className="h-10 w-10 object-contain ml-10"
+                    />
+                    <Link href="/" className="font-bold text-lg text-white">
+                        SAGIP PILIPINAS: HELP THOSE IN NEED!
+                    </Link>
+                </div>
+
+                <ul className="flex space-x-6 mr-10">
+                    {linksArray.map((link) => (
+                        <li key={link.href}>
+                            <Link
+                                href={link.href}
+                                className="text-white px-1.5 py-0.5 inline-block text-lg font-extralight transition-all ease-in-out duration-300 hover:text-white hover:decoration-white hover:underline hover:decoration-2 hover:underline-offset-8 hover:scale-110"
+                            >
+                                {link.displayName}
+                            </Link>
+                        </li>
+                    ))}
+                </ul>
+            </div>
+        </nav>
+    );
 };
 
 export default Navbar;
