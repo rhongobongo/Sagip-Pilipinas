3488742ea7f861b7979c2032ecb897f576d1998b
M4monski <162318730+M4monski@users.noreply.github.com>
2025-04-10 23:29:52 +0800

    Slight changes

--- src/app/(public)/donation/page.tsx
+++ src/app/(public)/donation/page.tsx
@@ -3,13 +3,25 @@ import DonationPageMap from '@/components/(page)/donationPage/donationPageMap';
 
 const DonationPage: React.FC = () => {
   return (
-    <div>
-      <div>
-        <DonationPageMap></DonationPageMap>
+    <div className="bg-white w-full h-screen text-black">
+      <div className="text-3xl font-semibold ml-[10%] pt-8">
+        <h1>MAKE DONATIONS</h1>
       </div>
-      <div>
-        {' '}
-        <DonationPageForm></DonationPageForm>
+      <div className="flex justify-center items-center">
+        <div>
+          <h2 className="text-xl pt-6">
+            Help disaster victims by making donations through filling up the
+            form below. Anywhere you are, lending a helping hand is always
+            possible.
+          </h2>
+        </div>
+        <div>
+          <DonationPageMap></DonationPageMap>
+        </div>
+        <div>
+          {' '}
+          <DonationPageForm></DonationPageForm>
+        </div>
       </div>
     </div>
   );
