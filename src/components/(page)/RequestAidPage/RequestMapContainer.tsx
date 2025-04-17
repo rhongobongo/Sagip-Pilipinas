671bed980fbca601cc17d13ddb2c557ceee054cd
22-1918-942 Keith Harvey Angel <keithharvey.angel@cit.edu>
2025-04-18 01:22:50 +0800

    (feat): Responsive RequestAid

--- src/components/(page)/RequestAidPage/RequestMapContainer.tsx
+++ src/components/(page)/RequestAidPage/RequestMapContainer.tsx
@@ -10,11 +10,13 @@ const RequestMapContainer = () => {
   const [pin, setPin] = useState<DefaultPin | null>(null);
 
   return (
-    <div className="grid bg-white">
-      <div className="text-black ml-8">
-        <h1 className="font-bold text-2xl mt-8 pl-16">REQUEST AID</h1>
-        <div className="flex items-center justify-center">
-          <p className="text-xl">
+    <div className="grid bg-[#fffdfd]">
+      <div className="text-black w-full">
+        <h1 className="text-lg font-bold sm:text-2xl mt-8 w-4/5 mx-auto sm:px-16 sm:w-full">
+          REQUEST AID
+        </h1>
+        <div className="flex items-center justify-center w-full">
+          <p className="text-base sm:text-xl w-4/5 mx-auto sm:w-full sm:mx-24">
             Request aid during or after a calamity by filling up this short form
             to alert our people to send help towards your location.
           </p>
